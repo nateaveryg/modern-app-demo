1ecfef22a82c5224bf5df60ef0dcb697f84cecdb
nateaveryg <averyn@gmail.com>
2024-03-05 06:27:00 +0000

    added be app

---
+++ be/main.py
@@ -0,0 +1,14 @@
+import os
+
+from flask import Flask, jsonify
+# from flask import Flask, jsonify
+
+
+app = Flask(__name__)
+
+@app.route('/hello', methods=['GET'])
+def index():
+    return jsonify({'message': 'Hello from service-a'})
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
