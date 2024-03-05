dc5b8fc9f35d94baee07150853d9bda7a6072d92
nateaveryg <averyn@gmail.com>
2024-03-05 17:35:45 +0000

    attempt at accessing hello_world.html

--- fe/main.py
+++ fe/main.py
@@ -5,14 +5,14 @@ import os
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return ('Hello, New World!')
-
-# to add 
 #@app.route("/")
 #def hello_world():
-#    return render_template("hello_world.html")
+#    return ('Hello, New World!')
+
+# to add 
+@app.route("/")
+def hello_world():
+    return render_template("./templates/hello_world.html")
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
@@ -24,4 +24,4 @@ def hello_api_dev():
     # Make a request to the BE service-a API.
     response = requests.get(f"{BASE_URL}/hello")
     print(response.json())
-
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
