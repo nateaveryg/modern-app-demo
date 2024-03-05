74d0664638e7d62a3a1d150699db7824839cb408
nateaveryg <averyn@gmail.com>
2024-03-05 04:56:19 +0000

    updated files for basic working app

--- fe/main.py
+++ fe/main.py
@@ -1,15 +1,15 @@
-from flask import Flask, render_template
-import jinja2
-import datetime
-import requests
+from flask import Flask, render_template, request
 import os
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    today = datetime.date.today()
-    return render_template("hello_world.html", today=today)
+    return ('Hello, World!')
+
+#@app.route("/")
+#def hello_world():
+#    return render_template("hello_world.html")
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
